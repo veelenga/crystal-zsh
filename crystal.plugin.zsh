1c3c4144a5d9cacaafaa3a8136653707a05f403c
Vitalii Elenhaupt <velenhaupt@gmail.com>
2015-10-26 18:23:52 +0200

    New aliases and bin/crystal

--- crystal.plugin.zsh
+++ crystal.plugin.zsh
@@ -1,5 +1,13 @@
+function _crystal_command () {
+  if [ -e "bin/crystal" ]; then
+    bin/crystal $@
+  else
+    command crystal $@
+  fi
+}
 
-# Aliases
+alias crystal='_crystal_command'
+
 alias cr='crystal'
 alias cri='crystal init'
 alias crb='crystal build'
@@ -7,3 +15,9 @@ alias crd='crystal deps'
 alias cre='crystal eval'
 alias crs='crystal spec'
 alias crt='crystal tool'
+alias crtb='crystal tool browser'
+alias crtc='crystal tool context'
+alias crtf='crystal tool format'
+alias crth='crystal tool hierarchy'
+alias crti='crystal tool implementations'
+alias crtt='crystal tool types'
